4aff4971b093196b1454f9bbd68f63c8829ee8f1
captiosus1 <86961653+captiosus1@users.noreply.github.com>
2021-11-01 13:04:57 +0530

    CHANGE

---
+++ src/application/Transport.java
@@ -0,0 +1,43 @@
+package application;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javafx.animation.PauseTransition;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Popup;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class Transport {
+	Pane view;
+	@FXML
+	public BorderPane borderpane; 
+
+	public void ShowStocks(ActionEvent e) {
+		FxmlLoader object = new FxmlLoader();
+		view = object.getPage("stocks");
+		borderpane.setCenter(view);
+	}
+}
