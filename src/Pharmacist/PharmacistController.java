f0918ecbd1335c28f5f4808c0cc18eadc33c620c
Vishal7780 <90459452+Vishal7780@users.noreply.github.com>
2021-12-30 11:18:34 +0530

    Changes in pharmacist

--- src/Pharmacist/PharmacistController.java
+++ src/Pharmacist/PharmacistController.java
@@ -3,16 +3,32 @@ package Pharmacist;
 import java.io.IOException;
 
 import Admin.AdminController;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 public class PharmacistController extends AdminController {
 	@FXML
-    public void Logout(ActionEvent e) throws IOException {
-		super.ShowHome(e);   	
+	public void Logout(ActionEvent e) throws IOException {
+		super.ShowHome(e);
 	}
+
 	@FXML
-    public void Exit() {
-		super.ExitApplication();   	
+	public void Exit() {
+		super.ExitApplication();
+
+	}
+
+	public void Sales(ActionEvent e) {
+
+		FxmlLoader3 object1 = new FxmlLoader3();
+		view = object1.getPage("Sales");
+		borderpane.setCenter(view);
+	}
+	public void Stocks(ActionEvent e) {
+
+		FxmlLoader3 object1 = new FxmlLoader3();
+		view = object1.getPage("Managing_stocks");
+		borderpane.setCenter(view);
 	}
 }
